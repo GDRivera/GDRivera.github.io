a7d64cdaf5b6842ac3a4b352327fe3ff5c9d3a3c
Gabriel Rivera <28474687+GDRivera@users.noreply.github.com>
2025-02-15 13:49:24 -0400

    asdasd

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '',
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  },
   plugins: [vue()],
 })
