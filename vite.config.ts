a276203bdf1eee4dcbe2990c9ead411e4c686a4b
Gabriel Rivera <28474687+GDRivera@users.noreply.github.com>
2025-02-15 13:54:28 -0400

    adasd

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,38 @@
-import { defineConfig } from 'vite'
+// Plugins
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'es',
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    }
-  },
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+        vuetify({
+            autoImport: true,
+        }),
+    ],
+    define: { 'process.env': {} },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
+    server: {
+        port: 3000,
+        open: true,
+        cors: true,
+    },
+    base: './',
 })
