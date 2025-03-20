4830498ff22e08b2f5ca903a41b7ac531b09f81e
Gabriel Rivera <28474687+GDRivera@users.noreply.github.com>
2025-03-19 20:30:59 -0400

    log

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const logs = [
     { date: '2025-03-16', message: 'It\'s a win today.' },
     { date: '2025-03-17', message: 'It\'s a win today. Walked for 30 mins.' },
     { date: '2025-03-18', message: 'Another win today. No exercise.' },
+    { date: '2025-03-19', message: 'GGs today. Ran 1 mile and walked (25 mins).'}
     // Add more logs as needed
 ];
 
